ef230db6726e9cce821c429f0027aa71f6eaa940
Naufal Rafif <odri.naufal@gmail.com>
2024-07-03 08:49:32 +0700

    new

--- day 6/exercises.js
+++ day 6/exercises.js
@@ -19,15 +19,32 @@ console.log(setaraOrNo({ a: 1, b: 2 }, { a: 1, b: 2 }));
 
 //2.function to get the intersection of two objects
 
-// function getIntersection(obj1, obj2) {
-//   return Object.keys(obj1).reduce((acc, key) => {
-//     if (obj2.hasOwnProperty(key) && obj1[key] === obj2[key]) {
-//       acc[key] = obj1[key];
-//     }
-//     return acc;
-//   }, {});
-// }
+function getIntersection(obj1, obj2) {
+  return Object.keys(obj1).reduce((acc, key) => {
+    if (obj2.hasOwnProperty(key) && obj1[key] === obj2[key]) {
+      acc[key] = obj1[key];
+    }
+    return acc;
+  }, {});
+}
 
-// const obj1 = { a: 1, b: 2 };
-// const obj2 = { a: 1, c: 3 };
-// console.log(getIntersection(obj1, obj2));
+const obj1 = { a: 1, b: 2 };
+const obj2 = { a: 1, c: 3 };
+console.log(getIntersection(obj1, obj2));
+
+
+//2.1
+
+function new(a,b){
+    let newObj = {}
+    for (let [key,val] of Object.entries(a)){
+        if (a[key]===b[key])
+            Object.assign(newObj, {[key]; val})
+    } 
+    
+}
+
+return newObj
+
+
+
