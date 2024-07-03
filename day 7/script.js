24fa5dab78d260314d4bc214596f84701d0a9d0e
Naufal Rafif <odri.naufal@gmail.com>
2024-07-03 10:43:15 +0700

    new

--- day 7/script.js
+++ day 7/script.js
@@ -33,3 +33,38 @@ let pekerja = {
 };
 
 pekerja.Gaji();
+
+// Factory Function
+/*
+regular Object
+
+let circle11 = {
+  radius:5,
+  locaton: {
+  x:10,
+  y:29,
+  }
+  draw: function (){
+  console.log("draw")
+  }
+}
+  circle.draw()
+  const circle1 = {...circle, radius: 200}
+
+*/
+
+function object(radius) {
+  return {
+    radius,
+    location: {
+      x: 10,
+      y: 20,
+    },
+    draw() {
+      console.log("draw");
+    },
+  };
+}
+
+let Circle1 = object(10);
+let Circle2 = object(20);
