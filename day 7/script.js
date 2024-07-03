1de15d559dc65b431ea1877fa5e03df087cf387d
Naufal Rafif <odri.naufal@gmail.com>
2024-07-03 11:58:52 +0700

    save

--- day 7/script.js
+++ day 7/script.js
@@ -1,70 +1,117 @@
-// let person = {
-//   name: "fulan",
-//   age: 2,
-//   gender: "male",
-//   geretPer: function () {
-//     console.log(`halo ${this.name}`);
+// // let person = {
+// //   name: "fulan",
+// //   age: 2,
+// //   gender: "male",
+// //   geretPer: function () {
+// //     console.log(`halo ${this.name}`);
+// //   },
+// // };
+// // person.geretPer();
+// // Object.keys(person);
+
+// //
+
+// let gaji = 30_000;
+// let overtime = 10;
+// let rate = 20;
+
+// function Gaji(gaji, overtime, rate) {
+//   return gaji + overtime * rate;
+// }
+
+// Gaji(gaji, overtime, rate);
+
+// // oop
+
+// let pekerja = {
+//   gaji: 30_000,
+//   overtime: 10,
+//   rate: 20,
+//   Gaji: function () {
+//     return this.gaji + this.over * this.rate;
 //   },
 // };
-// person.geretPer();
-// Object.keys(person);
 
-//
+// pekerja.Gaji();
 
-let gaji = 30_000;
-let overtime = 10;
-let rate = 20;
+// // Factory Function
+// /*
+// regular Object
 
-function Gaji(gaji, overtime, rate) {
-  return gaji + overtime * rate;
-}
+// let circle11 = {
+//   radius:5,
+//   locaton: {
+//   x:10,
+//   y:29,
+//   }
+//   draw: function (){
+//   console.log("draw")
+//   }
+// }
+//   circle.draw()
+//   const circle1 = {...circle, radius: 200}
 
-Gaji(gaji, overtime, rate);
+// */
 
-// oop
+// function object(radius) {
+//   return {
+//     radius,
+//     location: {
+//       x: 10,
+//       y: 20,
+//     },
+//     draw() {
+//       console.log("draw");
+//     },
+//   };
+// }
 
-let pekerja = {
-  gaji: 30_000,
-  overtime: 10,
-  rate: 20,
-  Gaji: function () {
-    return this.gaji + this.over * this.rate;
-  },
-};
+// let Circle1 = object(10);
+// let Circle2 = object(20);
 
-pekerja.Gaji();
+// Circle1.draw();
+// Circle2.draw();
 
-// Factory Function
-/*
-regular Object
+// //consturctor func
 
-let circle11 = {
-  radius:5,
-  locaton: {
-  x:10,
-  y:29,
-  }
-  draw: function (){
-  console.log("draw")
-  }
-}
-  circle.draw()
-  const circle1 = {...circle, radius: 200}
+// function Circle(radius) {
+//   this.radius = radius;
+//   this.draw = function () {
+//     console.log("draw");
+//   };
+// }
 
-*/
+// let circle3 = new Circle(10);
+// let circle4 = new Circle(20);
 
-function object(radius) {
-  return {
-    radius,
-    location: {
-      x: 10,
-      y: 20,
-    },
-    draw() {
-      console.log("draw");
-    },
+//abstruction
+
+function CircleAbs(radius) {
+  let defaultDrawing = "Dragon";
+  this.finalDrawing = `ini final drawing ${defaultDrawing}`;
+  this.radius = radius;
+  this.location = { x: 1, y: 5 };
+  this.draw = function () {
+    return this.defaultDrawing;
   };
+
+  Object.defineProperty(this, "defaultDrawing", {
+    get: function () {
+      return "defaultDrawing";
+    },
+    set: function (x) {
+      if (typeof x === "number") throw new Error(" this error");
+      defaultDrawing = x;
+    },
+  });
 }
 
-let Circle1 = object(10);
-let Circle2 = object(20);
+let circleAbs1 = new CircleAbs(5);
+console.log(circleAbs1.draw());
+console.log(circleAbs1);
+console.log(circleAbs1.defaultDrawing);
+console.log(circleAbs1.draw());
+circleAbs1.defaultDrawing = "kucing";
+console.log(circleAbs1.defaultDrawing);
+circleAbs1.radius = "string";
+console.log(circleAbs1.radius);
