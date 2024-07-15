6da4dcc7abc328cb7ede1a64ef17719ea51e838b
Naufal Rafif <odri.naufal@gmail.com>
2024-07-15 10:34:59 +0700

    new Json

---
+++ modul 2/day 1/LvL-2/ES-js/add.js
@@ -0,0 +1,6 @@
+import { sum, subtract, fibonacci, factorial } from "./property.js";
+
+console.log(sum(3, 4));
+console.log(subtract(5, 7));
+console.log(fibonacci(9, 10));
+console.log(factorial(8));
